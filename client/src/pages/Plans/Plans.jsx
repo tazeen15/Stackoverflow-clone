0d2e22f0404b81f70fc0ac956a2e6b20b03b2cfa
tazeen15 <75677052+tazeen15@users.noreply.github.com>
2023-04-12 00:45:13 +0530

    Update Plans.jsx

--- client/src/pages/Plans/Plans.jsx
+++ client/src/pages/Plans/Plans.jsx
@@ -1,53 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 import PricingCard from "./PricingCard";
 import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector} from "react-redux";
 
 import "./Plans.css";
 
- /*const Plans = () => {
-    const cardsData = [
-        {
-          id: 1,
-          type: 'basic',
-          title: 'Silver Plan',
-          description: 'Subscribe here to activate the silver plan',
-          price: 100.00,
-          mostPopular: false,
-          data: ['Having more than 1 doubt per day?', 'Ask 5 questions per day!']
-        },
-        {
-          id: 2,
-          type: 'medium',
-          title: 'Gold Plan',
-          description: 'Subscribe here to activate the gold plan.',
-          price: 1000.00,
-          mostPopular: true,
-          data: [`Still don't feel satisfied?`, 'Ask 10 questions per day!']
-        }
-    ];
 
-    const user = useSelector((state) => state.currentUserReducer);
-const navigate = useNavigate(); 
-    return (
-            /*user === null ? navigate("/Auth") :
-            <div className="app-wrapper">
-                <h2 style={{marginBottom: "20px"}}>Subscribe to a plan based on your needs and ask more doubts.</h2>
-                {
-                    cardsData.map(props => {                              
-                        return (
-                            <PricingCard
-                                {...props}
-                                key={props.id}
-                            />
-                        );
-                    })
-                } 
-                
-            </div> 
-);
-} */
 const Plans =() =>{
     const cardsData = [
         {
@@ -78,21 +37,13 @@ const Plans =() =>{
     return (
         <div className="app-wrapper">
             
-                {/* cardsData.map(props => {                              
-                    return (
-                        <PricingCard
-                            {...props}
-                            key={props.id}
-                        />
-                    );
-                }) */}
              
             {cardsData.map((user) => (
                 <PricingCard
                             {...user}
                             key={user.id}
                         />
-                        /*<div> <h1> {user.id}</h1></div>*/
+
       ))}
         </div> 
 );
